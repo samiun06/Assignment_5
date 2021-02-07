e49ae724e66066583665deb4d0d9d56811317af5
Samiun Jahan <samiunjahan06@gmail.com>
2021-02-07 13:01:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+const searchedFood = document.getElementById("searched-food");
+
+document.getElementById("search-button").addEventListener("click", () => {
+    document.getElementById("show-food").innerHTML = "";
+    displayFood(searchedFood.value);
+    searchedFood.value = "";
+});
+
+searchedFood.addEventListener("input", () =>{
+    document.getElementById("show-food").innerHTML = "";
+    displayFoodLetter(searchedFood.value);
+});
+
+const displayFood = foodName => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    .then(response => response.json())
+    .then(data => showFood(data))
+}
+
+const displayFoodLetter = foodLetter => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodLetter}`)
+    .then(response => response.json())
+    .then(data => showFood(data))
+}
+
+const showFood = data => {
+    const foodContainer = document.getElementById("show-food");
+    if (data.meals === null) {
+        const errorMessage = document.getElementById("error-message");
+        const h1 = document.createElement("h1");
+        h1.innerText = "Sorry. Searched food item not found. Please provide a valid food name";
+        h1.className = "error-text";
+        errorMessage.appendChild(h1);
+        errorMessage.style.display = "block";
+    }
+
+    else {
+        document.getElementById("error-message").style.display = "none";
+        data.meals.forEach(food => {
+            const singleItem = document.createElement("div");
+            singleItem.className = "card";
+            foodInfo = `
+                <img class="card-img-top" src="${food.strMealThumb}">
+                <h4 id="card-content">${food.strMeal}</h4>
+            `
+            singleItem.innerHTML = foodInfo;
+            foodContainer.appendChild(singleItem);
+    
+            singleItem.addEventListener("click", () => {
+                const inputField = document.getElementById("input-field");
+                const ul = document.createElement("ul");
+                const key = Object.keys(food);
+                const value = Object.values(food);
+                const ingredientArray = [];
+                const measureArrray = [];
+                foodContainer.style.display = "none";
+                inputField.style.display = "none";
+                const displayIngredients = document.getElementById("ingredients");
+                displayIngredients.style.display = "block";
+                ingredientContent = `
+                    <img src="${food.strMealThumb}">
+                    <h2 class="ingredient-title"><b>${food.strMeal}</b></h2>
+                    <h4>Ingredients</h4>
+                `
+                for(let i in key, value){
+                    if(key[i].startsWith("strIngredient") && value[i] != "" && value[i] != null){
+                        ingredientArray.push(key[i]);
+                    } else if(key[i].startsWith("strMeasure") && value[i] != "" && value[i] != null){
+                        measureArrray.push(key[i]);
+                    }
+                }
+            
+                for(let j = 0; j < ingredientArray.length; j++){
+                    const li = document.createElement("li");
+                    li.innerText = `${food[measureArrray[j]]} ${food[ingredientArray[j]]}`;
+                    ul.appendChild(li);
+                }
+    
+                const button = document.createElement("button");
+                button.innerText = "Back";
+                button.className = "back-btn";
+                button.addEventListener("click", () => {
+                    foodContainer.style.display = "grid";
+                    inputField.style.display = "block";
+                    displayIngredients.style.display = "none";
+                });
+    
+                displayIngredients.innerHTML = ingredientContent;
+                displayIngredients.appendChild(ul);
+                displayIngredients.appendChild(button);
+            });
+        });
+    }
+
+}
